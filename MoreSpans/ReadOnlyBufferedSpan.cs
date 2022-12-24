9118c165b04b64bd63a2b934bae6263e3d7507c6
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-12-24 17:50:07 -0600

    Add ReadOnlyBufferedSpan and constructors

---
+++ MoreSpans/ReadOnlyBufferedSpan.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MoreSpans;
+public readonly ref struct ReadOnlyBufferedSpan<Tfrom, Tto>
+{
+    private readonly Func<Tfrom, Tto> _funcFromBuffer;
+
+    public ReadOnlySpan<Tfrom> Span { get; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadOnlyBufferedSpan(ReadOnlySpan<Tfrom> span, Func<Tfrom, Tto> funcFromBuffer)
+    {
+        _funcFromBuffer = funcFromBuffer;
+        Span = span;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadOnlyBufferedSpan(ReadOnlySpan<Tfrom> span, int start, int length, Func<Tfrom, Tto> funcFromBuffer)
+    {
+        Span = span.Slice(start, length);
+        _funcFromBuffer = funcFromBuffer;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public unsafe ReadOnlyBufferedSpan(void* pointer, int length, Func<Tfrom, Tto> funcFromBuffer)
+    {
+        Span = new(pointer, length);
+        _funcFromBuffer = funcFromBuffer;
+    }
+}
