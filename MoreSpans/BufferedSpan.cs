adb632e7087f2bcab917c859b39d5e46c2ddf4b2
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-12-24 19:02:12 -0600

    Add BufferedSpan and its constructors

---
+++ MoreSpans/BufferedSpan.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MoreSpans;
+public readonly ref struct BufferedSpan<Tfrom, Tto>
+{
+    private readonly FromBufferFunc<Tfrom, Tto> _funcFromBuffer;
+    private readonly ToBufferFunc<Tfrom, Tto> _funcToBuffer;
+    private readonly int _size;
+
+    public Span<Tfrom> Span { get; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public BufferedSpan(Span<Tfrom> span, FromBufferFunc<Tfrom, Tto> funcFromBuffer, ToBufferFunc<Tfrom, Tto> funcToBuffer)
+    {
+        _funcFromBuffer = funcFromBuffer;
+        _funcToBuffer = funcToBuffer;
+        Span= span;
+        _size = Unsafe.SizeOf<Tto>() / Unsafe.SizeOf<Tfrom>();
+
+        if (_size == 0)
+            throw new NotSupportedException($"The first type arguement \"{typeof(Tfrom).Name}\" cannot be larger than the second type arguement \"{typeof(Tto).Name}\".");
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public BufferedSpan(Span<Tfrom> span, int start, int length, FromBufferFunc<Tfrom, Tto> funcFromBuffer, ToBufferFunc<Tfrom, Tto> funcToBuffer)
+        : this(span.Slice(start, length), funcFromBuffer, funcToBuffer) { }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public unsafe BufferedSpan(void* pointer, int length, FromBufferFunc<Tfrom, Tto> funcFromBuffer, ToBufferFunc<Tfrom, Tto> funcToBuffer)
+        : this(new(pointer, length), funcFromBuffer, funcToBuffer) { }
+}
