3e04370b703c7c021b78f6a333874a411edd52f7
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-12-24 18:15:17 -0600

    Update ReadOnlyBufferedSpan to use the new delegate and include a size check for Tfrom and Tto

--- MoreSpans/ReadOnlyBufferedSpan.cs
+++ MoreSpans/ReadOnlyBufferedSpan.cs
@@ -3,24 +3,27 @@
 namespace MoreSpans;
 public readonly ref struct ReadOnlyBufferedSpan<Tfrom, Tto>
 {
-    private readonly Func<Tfrom, Tto> _funcFromBuffer;
+    private readonly FromBufferFunc<Tfrom, Tto> _funcFromBuffer;
     private readonly int _size;
 
     public ReadOnlySpan<Tfrom> Span { get; }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ReadOnlyBufferedSpan(ReadOnlySpan<Tfrom> span, Func<Tfrom, Tto> funcFromBuffer)
+    public ReadOnlyBufferedSpan(ReadOnlySpan<Tfrom> span, FromBufferFunc<Tfrom, Tto> funcFromBuffer)
     {
         _funcFromBuffer = funcFromBuffer;
         Span = span;
         _size = Unsafe.SizeOf<Tto>() / Unsafe.SizeOf<Tfrom>();
+
+        if (_size == 0)
+            throw new NotSupportedException($"The first type arguement \"{typeof(Tfrom).Name}\" cannot be larger than the second type arguement \"{typeof(Tto).Name}\".");
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ReadOnlyBufferedSpan(ReadOnlySpan<Tfrom> span, int start, int length, Func<Tfrom, Tto> funcFromBuffer)
+    public ReadOnlyBufferedSpan(ReadOnlySpan<Tfrom> span, int start, int length, FromBufferFunc<Tfrom, Tto> funcFromBuffer)
         : this(span.Slice(start, length), funcFromBuffer) { }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public unsafe ReadOnlyBufferedSpan(void* pointer, int length, Func<Tfrom, Tto> funcFromBuffer)
+    public unsafe ReadOnlyBufferedSpan(void* pointer, int length, FromBufferFunc<Tfrom, Tto> funcFromBuffer)
         : this(new(pointer, length), funcFromBuffer) { }
 }
