9338cc196b60098a58f087de4bb7e8cc3aa6eb09
Stefan Kewatt <stefan.kewatt@gmail.com>
2022-12-24 19:01:58 -0600

    Add new delegate for writing to a buffered span

--- MoreSpans/SpanDelegates.cs
+++ MoreSpans/SpanDelegates.cs
@@ -3,3 +3,5 @@
 public delegate Tto ConvertFunc<Tfrom, Tto>(Tfrom value);
 
 public delegate Tto FromBufferFunc<Tfrom, Tto>(ReadOnlySpan<Tfrom> span);
+
+public delegate Tfrom[] ToBufferFunc<Tfrom, Tto>(Tto value);
